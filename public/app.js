951ca507c57a3681c8ce50bb2bae5dad93181ea7
SirawichDev <sirawit0676@gmail.com>
2020-06-04 01:58:20 +0700

    get api

--- public/app.js
+++ public/app.js
@@ -1,5 +1,27 @@
 const App = (props) => {
-  return <h1>DeNeacts :3 </h1>;
+  const [userJob, setUserJob] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/userjob")
+      .then((res) => res.json())
+      .then((data) => {
+        setUserJob(data);
+      });
+  }, []);
+
+  return (
+    <div class="center">
+      {userJob.map((ujob) => (
+        <div class="content" key={ujob.id}>
+          <img class="img" src={ujob.img} />
+          <h1>
+            {" "}
+            {ujob.name} - {ujob.job}
+          </h1>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
